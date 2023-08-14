a795a3bc96cf4217999e728e08f572512f0d9e5a
spykidua <spykidua@gmail.com>
2023-08-14 17:47:56 +0300

    change tsconfig

--- UI/itc-app/src/app/income-tax-calculator/calculation-result.model.ts
+++ UI/itc-app/src/app/income-tax-calculator/calculation-result.model.ts
@@ -1,8 +1,8 @@
 export class CalculationResult{
-    grossAnnualSalary: number = 0;
-    grossMonthlySalary: number = 0;
-    netAnnualSalary: number = 0;
-    netMonthlySalary: number = 0;
-    annualTaxPaid: number = 0;
-    monthlyTaxPaid: number = 0;
+    grossAnnualSalary: number;
+    grossMonthlySalary: number;
+    netAnnualSalary: number;
+    netMonthlySalary: number;
+    annualTaxPaid: number;
+    monthlyTaxPaid: number;
 }
